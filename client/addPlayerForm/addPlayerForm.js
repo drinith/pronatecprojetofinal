6f633a62737bc75fd38fa9d5d591451eca9bb2e8
Drinith <felpmel@gmail.com>
2018-08-04 17:36:54 -0300

    take picture

--- client/addPlayerForm/addPlayerForm.js
+++ client/addPlayerForm/addPlayerForm.js
@@ -1,4 +1,3 @@
-Meteor.subscribe('files.images.all');
 
 Template.addPlayerForm.events({
 
@@ -6,14 +5,10 @@ Template.addPlayerForm.events({
         event.preventDefault();
 
         let alunoNome = event.target.playerName.value;
-        let arquivo = event.target.pic.value;
-        console.log(arquivo);
         // let currentUserId = Meteor.userId();
         // let alunoNome = $('#inputName').val();
-
         // PlayersList.insert({ 'name': alunoNome, 'score': 0, 'criadoPor': currentUserId });
         Meteor.call("insertPlayerData", alunoNome);
-
 
         event.target.playerName.value = "";
     },
@@ -21,9 +16,7 @@ Template.addPlayerForm.events({
     'click .remove': function (event) {
 
         var idAluno = Session.get("selecionado");
-
         PlayersList.remove({ idAluno });
-
 
     },
 
@@ -32,51 +25,3 @@ Template.addPlayerForm.events({
 });
 
 
-//Template da imagem
-Template.uploadForm.onCreated(function () {
-    this.currentUpload = new ReactiveVar(false);
-});
-
-Template.uploadForm.helpers({
-    currentUpload: function () {
-        return Template.instance().currentUpload.get();
-    }
-});
-
-Template.uploadForm.events({
-    'change #fileInput': function (e, template) {
-        if (e.currentTarget.files && e.currentTarget.files[0]) {
-            // We upload only one file, in case 
-            // multiple files were selected
-            var upload = Images.insert({
-                file: e.currentTarget.files[0],
-                streams: 'dynamic',
-                chunkSize: 'dynamic'
-            }, false);
-
-            upload.on('start', function () {
-                template.currentUpload.set(this);
-            });
-
-            upload.on('end', function (error, fileObj) {
-                if (error) {
-                    alert('Error during upload: ' + error);
-                } else {
-                    alert('File "' + fileObj.name + '" successfully uploaded');
-                }
-                template.currentUpload.set(false);
-            });
-
-            upload.start();
-
-            console.log(upload);
-        }
-    }
-});
-
-Template.file.helpers({
-    imageFile: function () {
-      return Images.findOne();
-    },
-   
-  });
--- server/server_leaderboard.js
+++ server/server_leaderboard.js
@@ -1,22 +1,18 @@
 Meteor.publish('thePlayers', function () {
-    
+
     var currentUserId = Meteor.userId();
     console.log(currentUserId);
-    return PlayersList.find({criadoPor: currentUserId});
+    return PlayersList.find({ criadoPor: currentUserId });
 
 });
 
-Meteor.publish('files.images.all', function () {
-    return Images.find().cursor;
-  });
+
 
 Meteor.methods({
 
-    "insertPlayerData":function(alunoNome){
+    "insertPlayerData": function (alunoNome, imagem) {
         var currentUserId = Meteor.userId();
-        PlayersList.insert({ 'name': alunoNome, 'score': 0, 'criadoPor': currentUserId });
+        PlayersList.insert({ 'name': alunoNome, 'score': 0, 'criadoPor': currentUserId, 'imagem': imagem });
     },
 
-
-    
 });
